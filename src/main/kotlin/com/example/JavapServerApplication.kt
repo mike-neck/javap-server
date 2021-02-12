054f038f4862a576be2d068bae37ccd5fa7b3184
mike_neck <jkrt3333@gmail.com>
2021-02-12 21:30:46 +0900

    adjust error type

--- src/main/kotlin/com/example/JavapServerApplication.kt
+++ src/main/kotlin/com/example/JavapServerApplication.kt
@@ -45,7 +45,10 @@ class JavapController(
     val javaCode = deflaterService.inflateToJavaCode(base64)
       ?: return ResponseEntity.badRequest()
         .contentType(MediaType.APPLICATION_JSON)
-        .body(mapOf("error" to "invalid byte sequence or invalid base64 string"))
+        .body(mapOf(
+          "error" to "invalid byte sequence or invalid base64 string",
+          "cause" to "incompatible gzip format",
+        ))
 
     val (compileErrors, byteCode) = javacService.compileToBytes(javaCode)
     if (byteCode == null) {
@@ -53,21 +56,30 @@ class JavapController(
         .contentType(MediaType.APPLICATION_JSON)
         .body(mapOf(
           "error" to "failed to compile",
-          "cause" to compileErrors.joinToString(",\n") { "${it.type} ${it.message} at line=${it.lineNumber} col=${it.columnNumber}" }
+          "cause" to compileErrors.joinToString(",\n") { "${it.type} ${it.message} at line=${it.lineNumber} col=${it.columnNumber}" },
         ))
     }
 
     val path = fileService.createTemporaryFile(byteCode) 
       ?: return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .contentType(MediaType.APPLICATION_JSON)
-        .body(mapOf("error" to "failed to write bytecode of ${byteCode.first}"))
+        .body(mapOf(
+          "error" to "failed to write bytecode of ${byteCode.first}",
+          "cause" to "server error",
+        ))
 
     val (success, contents) = javapService.runJavap(path)
     return when(success) {
-      true -> ResponseEntity.ok().body(mapOf("result" to "success", "contents" to contents))
+      true -> ResponseEntity.ok().body(mapOf(
+        "result" to "success",
+        "contents" to contents,
+      ))
       false -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .contentType(MediaType.APPLICATION_JSON)
-        .body(mapOf("error" to "failed to execute javap", "contents" to contents))
+        .body(mapOf(
+          "error" to "failed to execute javap",
+          "cause" to contents,
+        ))
     }
   }
 }
